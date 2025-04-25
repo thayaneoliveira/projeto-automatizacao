6a389e0c74472bd81329be095a452d9826d2f29e
Thayane Oliveira <thay.skaywalker@gmail.com>
2025-04-25 12:24:40 -0400

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
-  video: true,
+  e2e: {
+    video: true,
+    screenshotOnRunFailure: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
+    reporter: "mochawesome", // Especificar mochawesome aqui
+    reporterOptions: {
+      reportDir: "cypress/results", // Diretório para armazenar os relatórios
+      overwrite: true,
+      html: true,
+      json: true,
+    },
+  },
 });
