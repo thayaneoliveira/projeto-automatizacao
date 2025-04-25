222a8ce7fd32b7b28bf62c2d3ea0bc14747a27a0
Thayane Oliveira <thay.skaywalker@gmail.com>
2025-04-25 14:36:23 -0400

    Acionando o fluxo de trabalho do Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,18 +3,22 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
+  reporter: "mochawesome", // ✅ fora do bloco 'e2e'
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: true,
+    html: true,
+    json: true,
+  },
   e2e: {
     projectId: "qe36i8",
     video: true,
     screenshotOnRunFailure: true,
     screenshotsFolder: "cypress/screenshots",
     videosFolder: "cypress/videos",
-    reporter: "mochawesome", // Especificar mochawesome aqui
-    reporterOptions: {
-      reportDir: "cypress/results", // Diretório para armazenar os relatórios
-      overwrite: true,
-      html: true,
-      json: true,
+    setupNodeEvents(on, config) {
+      // aqui vai qualquer evento de plugin se necessário
+      return config;
     },
   },
 });
